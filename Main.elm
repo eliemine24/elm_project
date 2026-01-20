4a4b2f7c7340209630b653224a08fa6a5317ec62
eliemine24 <elie.gautier24@gmail.com>
2026-01-20 14:30:08 +0100

    page minimaliste

--- Main.elm
+++ Main.elm
@@ -2,3 +2,34 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (Html, div, text)
+
+type alias Model =
+    { message : String }
+
+
+type Msg
+    = NoOp
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { message = "GuessIt démarre !" }, Cmd.none )
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text model.message ]
+
+main : Program () Model Msg
+main =
+    Browser.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = \_ -> Sub.none
+        }
